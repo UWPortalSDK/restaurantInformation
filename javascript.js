abe791e1f74e7a3c871ebd64ef1aeea21ec7bdf4
Mykhaylo (Misha) Ivanovych Kuzma <mishakuzma@users.noreply.github.com>
2016-10-29 12:17:11 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -75,6 +75,49 @@ angular.module('portalApp')
 			details: 'Comfy eatery serving a diverse menu of mains like ratatouille, burgers, steak & raw vegan pad Thai.',
 			menu: 'https://marblesuptown.com/menu-dinner.html',
 			schedule: '11:30 - 10, Fri: 11:30 - 11, Sun: 11:30 - 9'
+		},
+        {
+			title:'East Side Marios',
+			tags: ['Take-Out'],
+			details: '',
+            schedule: '11 am - 11pm everyday',
+            menu:'http://www.eastsidemarios.com/menu/main/starters/'
+		},
+		{
+			title:'Wild Wing',
+			tags: ['Take-Out'],
+			details: 'Mauris cursus, sapien et malesuada ultrices, purus sapien iaculis tellus, quis semper magna est at leo.',
+			schedule: ""
+        },
+		{
+			title:'Swiss Chalet',
+			tags: ['Take-Out', 'Fast Food', 'Sit-Down'],
+			details: 'Donec id quam eu odio feugiat sagittis. Duis a tempus neque. Praesent elementum quis ante quis commodo. Sed tincidunt aliquet dolor sit amet laoreet. '
+		},
+		{
+			title:'Manchu Wok',
+			tags: ['Take-out'],
+			details: 'Proin sem quam, rutrum id ante id, scelerisque tempor quam. Curabitur pharetra turpis at sem placerat, non vehicula ligula tincidunt.'
+		},
+		{
+			title:'Spring Rolls',
+			tags: ['Take-out', 'Fast food'],
+            schedule: '',
+            menu: 'http://www.springrolls.ca/menu.php',
+			details: 'Mauris nec ultricies metus. Cras et dictum justo. Nam a ullamcorper dolor. Cras fringilla metus vel facilisis vehicula.'
+		},
+		{
+			title:'China Legend',
+			tags: ['Take-out', 'Sit-Down'],
+            schedule: '11am - 10pm M-Thurs., 11am - 10:30pm F, 12-10:30pm S - Sun',
+			details: 'Located in the beautiful city of Waterloo, our restaurant has been dedicated to offering the most memorable dining experience for you.'
+		},
+        {
+			title:'Empress of India',
+			tags: ['Take-out', 'Sit-Down'],
+            schedule: '11:30am - 2pm then 5pm-10pm M-Thurs., 11:30am - 2pm then 5pm-10:30pm F-Sat, 4-9pm Sun',
+			details: 'Casual restaurant with classic Indian dishes, a bar serving cocktails & a weekday lunch buffet.',
+            menu: 'http://empressofindia.ca/menu/main-menu/appetizers'
 		}
 	];
 	
