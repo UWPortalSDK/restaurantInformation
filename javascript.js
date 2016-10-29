e6e9c262f95c164bcfd118cd09e7db944b9af8be
Mykhaylo (Misha) Ivanovych Kuzma <mishakuzma@users.noreply.github.com>
2016-10-29 12:28:18 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -3,37 +3,6 @@ angular.module('portalApp')
 	
 	// restaurant list - hardcoded
 	$scope.items = [
-		{
-			title:'East Side Marios',
-			tags: ['Take-Out'],
-			details: 'Menu: \n test',
-            schedule: "9 - 5"
-		},
-		{
-			title:'Wild Wing',
-			tags: ['Take-Out'],
-			details: 'Mauris cursus, sapien et malesuada ultrices, purus sapien iaculis tellus, quis semper magna est at leo.'
-		},
-		{
-			title:'Swiss Chalet',
-			tags: ['Take-Out', 'Fast Food', 'Sit-Down'],
-			details: 'Donec id quam eu odio feugiat sagittis. Duis a tempus neque. Praesent elementum quis ante quis commodo. Sed tincidunt aliquet dolor sit amet laoreet. '
-		},
-		{
-			title:'Manchu Wok',
-			tags: ['Take-out'],
-			details: 'Proin sem quam, rutrum id ante id, scelerisque tempor quam. Curabitur pharetra turpis at sem placerat, non vehicula ligula tincidunt.'
-		},
-		{
-			title:'Spring Rolls',
-			tags: ['Take-out', 'Fast food'],
-			details: 'Mauris nec ultricies metus. Cras et dictum justo. Nam a ullamcorper dolor. Cras fringilla metus vel facilisis vehicula.'
-		},
-		{
-			title:'',
-			tags: [''],
-			details: 'Curabitur scelerisque lorem risus, in luctus orci hendrerit non. Praesent quis tellus dapibus dolor consectetur volutpat.'
-		},
         {
 			title:'Aunty\'s Kitchen',
 			tags: ['Sit-in'],
